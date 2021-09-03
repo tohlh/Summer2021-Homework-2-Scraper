edacf99780766a1719e34d03929fbc9d1bf3a2c2
tohlh <tohliheng@gmail.com>
2021-09-03 10:17:19 +0800

    use new objects for every new scrape

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ import videoPage
 
 da = dbAdapter.adapter()
 ms = mainPage.mainPageScraper()
-cs = channelPage.channelScraper()
-vs = videoPage.videoScraper()
 
 # Configuration
-channelsToScrape = 10
-videosToScrape = 5000
+channelsToScrape = 300
 
 def main():
     print("Starting to scrape main page for channel links...")
@@ -20,10 +17,10 @@ def main():
         totalChannelLinks = da.getTotalChannelsToScrape()
     print("Found {0} unique channels!".format(totalChannelLinks))
     
-    #currChannel = da.getUnscrapedChannel()
-    currChannel = ''
+    currChannel = da.getUnscrapedChannel()
     while currChannel != '':
         print('Scraping channel ID: {0}... ({1}/{2})'.format(currChannel, da.getScrapedChannelsNum() + 1, da.getTotalChannelsToScrape()))
+        cs = channelPage.channelScraper()
         cs.scrapeChannel(currChannel)
         currChannel = da.getUnscrapedChannel()
         print('Done!')
@@ -31,6 +28,7 @@ def main():
     currVideo = da.getUnscrapedVideo()
     while currVideo != '':
         print('Scraping video ID: {0}... ({1}/{2})'.format(currVideo, da.getScrapedVideosNum() + 1, da.getTotalVideosToScrape()))
+        vs = videoPage.videoScraper()
         vs.scrapeVideo(currVideo)
         currVideo = da.getUnscrapedVideo()
         print('Done!')
